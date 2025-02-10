597efa9e5f1c542717cf5b61e322611160228f44
SamYuan1990 <yy19902439@126.com>
2025-02-10 14:28:18 +0800

    fix up
    Signed-off-by: SamYuan1990 <yy19902439@126.com>

--- src/onceoffTasks/cve.js
+++ src/onceoffTasks/cve.js
@@ -115,8 +115,10 @@ async function fromCVEToPodDeployment() {
 
   // 提取所有的 vulnerabilities.id 字段
   const vulnerabilityIds = new Set() // 使用 Set 去重
+  // eslint-disable-next-line github/array-foreach
   data.packages.forEach(content => {
     core.info(`scan package ${content}`)
+    // eslint-disable-next-line github/array-foreach
     content.vulnerabilities.forEach(vulnerability => {
       vulnerabilityIds.add(vulnerability.id)
     })
