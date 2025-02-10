49a100ac04f64720f281913ae0f67bdf3c16b703
SamYuan1990 <yy19902439@126.com>
2025-02-10 14:58:23 +0800

    fix up
    Signed-off-by: SamYuan1990 <yy19902439@126.com>

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 /**
  * The entrypoint for the action.
  */
-const { run } = require('./main')
+const { runAst } = require('./main')
 const core = require('@actions/core')
 const OpenAI = require('openai')
 const { cvss_deployment } = require('./onceoffTasks/cvssDeployment')
 const { processOutput } = require('./outputhandler/generalOutputProcessor')
 
-async function start() {
+async function run() {
   const baseURL = core.getInput('baseURL', { required: true })
   core.info(`We are going to talk with Gen AI with URL ${baseURL}`)
 
@@ -47,11 +47,11 @@ async function start() {
     LLMresponses = cvss_deployment(openai, model_parameters, dryRun)
   } else {
     // AST tasks
-    run(openai, model_parameters, control_group, dryRun)
+    runAst(openai, model_parameters, control_group, dryRun)
   }
   processOutput(LLMresponses)
   // Log the current timestamp, wait, then log the new timestamp
   core.debug('complete at:', new Date().toTimeString())
 }
 
-start()
+run()
