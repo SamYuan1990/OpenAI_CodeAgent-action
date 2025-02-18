646893d240fc40e6de279be6f3bb833a6a6a5037
SamYuan1990 <yy19902439@126.com>
2025-02-18 20:15:49 +0800

    fix up
    Signed-off-by: SamYuan1990 <yy19902439@126.com>

--- dist/index.js
+++ dist/index.js
@@ -14459,17 +14459,18 @@ function buildGoAST() {
   return new Promise((resolve, reject) => {
     // 解析相对路径为绝对路径
     // 构建 Go 项目的命令
-    const command = `wget https://github.com/SamYuan1990/OpenAI_CodeAgent-action/blob/main/goASTBin -O ./goASTBin && chmod a+x goASTBin`
+    const command = `wget https://raw.githubusercontent.com/SamYuan1990/OpenAI_CodeAgent-action/refs/heads/main/goASTBin
+ -O ./goASTBin && chmod a+x goASTBin`
 
     // 执行命令
     exec(command, (error, stdout, stderr) => {
       if (error) {
-        core.error(`fail to build Go project: ${error}`)
+        core.error(`fail to download go binary: ${error}`)
         reject(`构建 Go 项目失败: ${error}`)
         return
       }
       if (stderr) {
-        core.error(`fail to build Go output: ${error}`)
+        core.error(`fail to download go binary: ${error}`)
         reject(`构建 Go 项目输出错误: ${stderr}`)
         return
       }
