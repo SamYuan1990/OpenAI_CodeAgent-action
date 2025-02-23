5a9b8ee6fccb47e774a87b8360b1bd82ea9e77d2
SamYuan1990 <yy19902439@126.com>
2025-02-23 10:29:05 +0800

    decouple output from core.output
    Signed-off-by: SamYuan1990 <yy19902439@126.com>

--- dist/index.js
+++ dist/index.js
@@ -40103,6 +40103,12 @@ function processOutput(LLMresponses) {
   // prompt_precent, > file content, avg output
   // content_precent > file content, avg output
   // }
+  const Output = {
+    avg_prompt_precent: 0,
+    avg_content_precent: 0,
+    LLMresponse: '',
+    final_prompt: ''
+  }
   const folderName = './GenAI_output'
   logger.Info('make output dir')
   fs.mkdirSync(folderName, { recursive: true })
@@ -40127,15 +40133,18 @@ function processOutput(LLMresponses) {
   const avg_prompt_precent = prompt_precent_sum / LLMresponses.length
   const avg_content_precent = content_precent_sum / LLMresponses.length
   // Set Action output
-  core.setOutput('avg_prompt_precent', avg_prompt_precent)
   logger.Info('avg_prompt_precent', avg_prompt_precent)
-  core.setOutput('avg_content_precent', avg_content_precent)
+  Output.avg_prompt_precent = avg_prompt_precent
   logger.Info('avg_content_precent', avg_content_precent)
+  Output.avg_content_precent = avg_content_precent
   if (LLMresponses.length === 1) {
     logger.Info(LLMresponses[0])
-    core.setOutput('LLMresponse', LLMresponses[0].response)
-    core.setOutput('final_prompt', LLMresponses[0].final_prompt)
+    Output.LLMresponse = LLMresponses[0].response
+    Output.final_prompt = LLMresponses[0].final_prompts
   }
+  const filePath = path.join(folderName, `summary.json`)
+  const summary_jsonString = JSON.stringify(Output, null, 2)
+  fs.writeFileSync(filePath, summary_jsonString)
 }
 
 module.exports = {
