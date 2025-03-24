fb0e6e2e9d3604c2fcdd67f7b78abdd6e4d32110
SamYuan1990 <yy19902439@126.com>
2025-03-24 15:33:37 +0800

    fix up
    Signed-off-by: SamYuan1990 <yy19902439@126.com>

--- src/onceoffTasks/cve_code.js
+++ src/onceoffTasks/cve_code.js
@@ -9,6 +9,10 @@ const { JustInvokeAI } = require('../aiagent')
 
 function grepSync(pattern, filePath) {
   try {
+    const grep_result = {
+      matches: [],
+      files: []
+    }
     // 执行 grep 命令并获取输出
     const grepcmd = `grep ${pattern} -rw ${filePath} --exclude-dir=node_modules --exclude-dir=vendor --exclude-dir=.git --exclude=sbom.json --exclude=syft --exclude=cve.json`
     logger.Info(grepcmd)
@@ -19,11 +23,11 @@ function grepSync(pattern, filePath) {
     const fileList = result
       .map(line => line.split(':')[0]) // 提取文件名（grep 输出格式为 "filename:matched content"）
       .filter((file, index, self) => self.indexOf(file) === index) // 去重
-
-    return {
-      matches: result, // 匹配结果
-      files: fileList // 文件列表
+    if (result.length > 0) {
+      grep_result.matches = result
+      grep_result.files = fileList
     }
+    return grep_result
   } catch (error) {
     // 如果命令执行失败，返回空数组或抛出错误
     // console.error(`Error: ${error.stderr.toString()}`)
