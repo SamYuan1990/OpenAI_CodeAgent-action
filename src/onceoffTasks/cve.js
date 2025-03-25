c39416fb01bd90c66e654cacfafb2a113922db2d
SamYuan1990 <yy19902439@126.com>
2025-03-25 16:35:47 +0800

    refactor CVE tools
    Signed-off-by: SamYuan1990 <yy19902439@126.com>

--- src/onceoffTasks/cve.js
+++ src/onceoffTasks/cve.js
@@ -2,7 +2,7 @@
 const fs = require('fs')
 const path = require('path')
 const { logger } = require('../utils/logger')
-
+const { collectInformation } = require('../tools/cvetools')
 // CVSS 3.1 评分指标
 const cvss_3_1_metrics = {
   // Base Metrics - Exploitability
@@ -58,46 +58,6 @@ const cvss_3_1_metrics = {
   'Modified Integrity (MI)': ['Not Defined', 'None', 'LOW', 'HIGH'],
   'Modified Availability (MA)': ['Not Defined', 'None', 'LOW', 'HIGH']
 }
-async function fetchSeverityScoreBreakdown(url) {
-  try {
-    // 发送HTTP GET请求
-    logger.Info(`start to fetch CVE details ${url}`)
-    const cveawg_data = await fetchCveData(url)
-    const cveawg_json = JSON.parse(cveawg_data)
-    let cvssMetrics = null
-    for (let i = 0; i < cveawg_json.containers.adp.length; i++) {
-      if (
-        cveawg_json.containers.adp[i]['metrics'] !== null &&
-        cveawg_json.containers.adp[i]['metrics'] !== undefined
-      ) {
-        cvssMetrics = cveawg_json.containers.adp[i].metrics[0]
-        logger.Info(
-          `cvss info from web ${cveawg_json.containers.adp[i].metrics[0].cvssV3_1.vectorString}`
-        )
-        break
-      }
-    }
-    if (cvssMetrics && cvssMetrics.cvssV3_1) {
-      const cvssV3_1 = cvssMetrics.cvssV3_1
-      return {
-        //baseSeverity: cvssV3_1.baseSeverity,
-        'Attack vector': cvssV3_1.attackVector,
-        'Attack complexity': cvssV3_1.attackComplexity,
-        'Privileges required': cvssV3_1.privilegesRequired,
-        'User interaction': cvssV3_1.userInteraction,
-        Scope: cvssV3_1.scope,
-        Confidentiality: cvssV3_1.confidentialityImpact,
-        'Integrity impact': cvssV3_1.integrityImpact,
-        'Availability impact': cvssV3_1.availabilityImpact
-      }
-    } else {
-      return null
-    }
-  } catch (error) {
-    logger.Info(`Error fetching ${url}:`, error)
-    return null
-  }
-}
 
 let init = false
 const myMetrics = {
@@ -121,66 +81,30 @@ const myMetrics = {
 
 async function fromCVEToPodDeployment(control_group) {
   logger.Info(`start process CVE to pod deployment`)
-  // 读取 JSON 文件
-  const data = JSON.parse(
-    fs.readFileSync(`${control_group.dirPath}/cve.json`, 'utf8')
-  )
-
-  // 提取所有的 vulnerabilities.id 字段
-  const vulnerabilityIds = new Set() // 使用 Set 去重
-  // eslint-disable-next-line github/array-foreach
-  data.packages.forEach(content => {
-    logger.Info(`scan package ${content}`)
-    // eslint-disable-next-line github/array-foreach
-    content.vulnerabilities.forEach(vulnerability => {
-      vulnerabilityIds.add(vulnerability.id)
-    })
-  })
-
-  // 将结果转换为数组
-  const uniqueVulnerabilityIds = Array.from(vulnerabilityIds)
-
-  // 生成 URL 链接
-  const cveUrls = uniqueVulnerabilityIds.map(
-    cveId => `https://cveawg.mitre.org/api/cve/${cveId}`
-  )
-
-  for (const url of cveUrls) {
-    const cvePattern = /CVE-\d{4}-\d+/
-    const match = url.match(cvePattern)
-    const cveInfo = match ? match[0] : null
-
-    if (cveInfo === null) {
-      continue
-    }
-
-    const severityScoreBreakdown = await fetchSeverityScoreBreakdown(url)
-
-    if (severityScoreBreakdown) {
+  const information = await collectInformation(control_group)
+  for (let i = 0; i < information.length; i++) {
+    if (information[i].CVEscore) {
       if (!init) {
         init = true
-        for (const [key, value] of Object.entries(severityScoreBreakdown)) {
+        for (const [key, value] of Object.entries(information[i].CVEscore)) {
           myMetrics[key] = value
-          myMetrics[`${key} from`] =
-            `https://www.cve.org/CVERecord?id=+${cveInfo}`
+          myMetrics[`${key} from`] = information[i].cveLink
         }
       } else {
-        for (const [key, value] of Object.entries(severityScoreBreakdown)) {
+        for (const [key, value] of Object.entries(information[i].CVEscore)) {
           const cValue = myMetrics[key]
           if (
             cvss_3_1_metrics[key].indexOf(cValue) >
             cvss_3_1_metrics[key].indexOf(value)
           ) {
             myMetrics[key] = value
-            myMetrics[`${key} from`] =
-              `https://www.cve.org/CVERecord?id=+${cveInfo}`
+            myMetrics[`${key} from`] = information[i].cveLink
           }
           if (
             cvss_3_1_metrics[key].indexOf(cValue) ===
             cvss_3_1_metrics[key].indexOf(value)
           ) {
-            myMetrics[`${key} from`] +=
-              `,https://www.cve.org/CVERecord?id=+${cveInfo}`
+            myMetrics[`${key} from`] += `,${information[i].cveLink}`
           }
         }
       }
@@ -193,48 +117,12 @@ async function fromCVEToPodDeployment(control_group) {
   let cvssStr = ''
   for (const [metric, values] of Object.entries(myMetrics)) {
     if (!metric.includes('from')) {
-      cvssStr += ` ${metric}:${values},`
+      cvssStr += `${metric}:${values},`
     }
   }
   return cvssStr
 }
 
-const https = require('https')
-
-function httpsGetPromise(options) {
-  return new Promise((resolve, reject) => {
-    const req = https.request(options, res => {
-      let data = ''
-      res.on('data', chunk => {
-        data += chunk
-      })
-      res.on('end', () => {
-        resolve(data)
-      })
-    })
-    req.on('error', e => {
-      reject(e)
-    })
-    req.end()
-  })
-}
-
-async function fetchCveData(url) {
-  try {
-    const options = {
-      hostname: new URL(url).hostname,
-      path: new URL(url).pathname + new URL(url).search,
-      method: 'GET'
-    }
-    const data = await httpsGetPromise(options)
-    return data
-  } catch (error) {
-    throw error
-  }
-}
-
 module.exports = {
-  fromCVEToPodDeployment,
-  fetchSeverityScoreBreakdown,
-  fetchCveData
+  fromCVEToPodDeployment
 }
